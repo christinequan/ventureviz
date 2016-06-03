cd3e3b58d855d15938d84f4cdfcdee63c8d86e93
Katherine Phan <kxphan@stanford.edu>
2016-06-03 01:53:07 -0700

    Added amount slider. STILL NEED AXES.

--- VCviz.js
+++ VCviz.js
@@ -14,22 +14,19 @@ $(function() {
       " - " + (new Date($( "#slider-range" ).slider( "values", 1 )*1000)).toDateString());
 });
 
-/*
-
 $(function() {
-    $( "#slider-range" ).slider({
+    $( "#amt-range" ).slider({
       range: true,
-      min: new Date('1997.05.14').getTime() / 1000,
-      max: new Date('2015.12.15').getTime() / 1000,
-      step: 86400,
-      values: [ new Date('2011.01.01').getTime() / 1000, new Date('2013.02.01').getTime() / 1000 ],
+      min: 0,
+      max: 15000000,
+      step: 1000000,
+      values: [ 2500000, 10000000 ],
       slide: function( event, ui ) {
-        $( "#amount" ).val( (new Date(ui.values[ 0 ] *1000).toDateString() ) + " - " + (new Date(ui.values[ 1 ] *1000)).toDateString() );
+        $( "#raised" ).val( ui.values[ 0 ] + " - " + + ui.values[ 1 ] );
         
       }
     });
-    $( "#amount" ).val( (new Date($( "#slider-range" ).slider( "values", 0 )*1000).toDateString()) +
-      " - " + (new Date($( "#slider-range" ).slider( "values", 1 )*1000)).toDateString());
+    $( "#raised" ).val($( "#amt-range" ).slider( "values", 0 ) +
+              "-" + $( "#amt-range" ).slider( "values", 1 ));
 });
 
-*/

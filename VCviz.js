e0f600e14320ed14ba225506264d2e21287b53c6
Katherine Phan <kxphan@stanford.edu>
2016-06-03 03:45:38 -0700

    Filters working.

--- VCviz.js
+++ VCviz.js
@@ -1,3 +1,4 @@
+
 $(function() {
     $( "#slider-range" ).slider({
       range: true,
@@ -18,15 +19,75 @@ $(function() {
     $( "#amt-range" ).slider({
       range: true,
       min: 0,
-      max: 15000000,
-      step: 1000000,
-      values: [ 2500000, 10000000 ],
+      max: 150000000,
+      step: 5000000,
+      values: [ 10000000, 100000000 ],
       slide: function( event, ui ) {
         $( "#raised" ).val( ui.values[ 0 ] + " - " + + ui.values[ 1 ] );
+        raised_range[0] = $("#amt-range").slider( "values", 0);
+        raised_range[1] = $("#amt-range").slider( "values", 1);
+
+        scale = d3.scale.linear()
+            .domain([0, d3.max(data, function(d) { return parseInt(d['raised_amount_usd']); })])
+            .range([0, MAX_BAR_HEIGHT]);
         
+        // doing some bad coding here
+        d3.selectAll("svg > *").remove();
+        svg.selectAll("rect")
+            .data(data)
+            .enter()
+            .append("rect")
+            .attr("width", function() {
+                return w / data.length;
+            })
+            .attr("height", function(d) {
+                return scale(d.raised_amount_usd) + "px";
+            })
+            .attr("x", function(d, i) {
+                return i * ((w / data.length) + 1);
+            })
+            .attr("y", function(d) {
+                return h - scale(d.raised_amount_usd);
+            })
+
+            .attr("fill", function(d) {
+              if ((d.raised_amount_usd > raised_range[0]) && (d.raised_amount_usd < raised_range[1])) {
+                //console.log(d.raised_amount_usd)
+                     if (d.funding_round_type == "seed") {
+                        return "#edf8b1";
+                    } else if (d.funding_round_type == "venture") {
+                        if (d.funding_round_code == "A") {
+                            return "#c7e9b4";
+                        } else if (d.funding_round_code == "B") {
+                            return "#7fcdbb";
+                        } else if (d.funding_round_code == "C") {
+                            return "#41b6c4";
+                        } else if (d.fnding_round_code == "D") {
+                            return "#1d91c0";
+                        } else if (d.funding_round_code == "E") {
+                            return "#225ea8";
+                        } else if (d.funding_round_code == "F") {
+                            return "#253494";
+                        } else {
+                            return "#081d58";
+                        }
+                    } else {                                            // angel
+                        return "#ffffd9";
+                    }
+              }
+                  else { 
+                    return "#ffffff";}
+            })
+            .on('mouseover', function(d) {
+                colorOver.call(this,d);
+                tip.show(d);
+            })
+            .on('mouseout', function(d) {
+                colorOut.call(this,d);
+                tip.hide(d);
+            });
       }
     });
     $( "#raised" ).val($( "#amt-range" ).slider( "values", 0 ) +
               "-" + $( "#amt-range" ).slider( "values", 1 ));
 });
-
